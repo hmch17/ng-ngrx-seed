a9da274495dd3814c41d95f0ea928ac5867341b2
HMChang <ahuang17@gmail.com>
2017-09-30 23:32:21 +0200

    Passing the test

--- src/app/store/cookbook.service.spec.ts
+++ src/app/store/cookbook.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CookbookService } from './cookbook.service';
+import { Store } from '@ngrx/store';
+import { StoreStub } from '../test/store-stub.spec';
 
 describe('CookbookService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CookbookService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                CookbookService,
+                { provide: Store, useClass: StoreStub }
+            ]
+        });
     });
-  });
 
-  it('should be created', inject([CookbookService], (service: CookbookService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([ CookbookService ], (service: CookbookService) => {
+        expect(service).toBeTruthy();
+    }));
 });
---
+++ src/app/test/store-stub.spec.ts
@@ -0,0 +1,4 @@
+export class StoreStub {
+    dispatch = jasmine.createSpy('dispatch');
+    select = jasmine.createSpy('select');
+}
